69a2458f594fcde7111a01d9fef6ebe8c4f0f619
daniel <danieldocs25@gmail.com>
2025-09-16 11:57:55 -0300

    comitt

---
+++ entregas/Atividade 17/calculadora.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _on_button_pressed() -> void:
+
+	$Label.text = str($OptionButton.get_selected_id())
+	if $OptionButton.get_selected_id() == 0:
+		$Label.text = str(float($numero1.text) + float($numero2.text))
+
+	elif $OptionButton.get_selected_id() == 1:
+		$Label.text = str(float($numero1.text) - float($numero2.text))
+
+	elif $OptionButton.get_selected_id() == 2:
+		$Label.text = str(float($numero1.text) * float($numero2.text))
+
+	elif $OptionButton.get_selected_id() == 3:
+		$Label.text = str(float($numero1.text) / float($numero2.text))
+
+	else:
+		$Label.text = "Selecione uma operação ou os números!"
